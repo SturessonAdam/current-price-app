127098f35c3fc095dc495f8745207b62344f4442
Adam <Adam.sturesson@live.se>
2025-03-16 16:46:07 +0100

    Fetch and display electricity prices with loading indicator

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,24 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, ActivityIndicator } from "react-native";
 
 export default function Index() {
+  const [prices, setPrices] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://192.168.68.108:8080/prices")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Fetched data:", data); // Kolla om det finns några priser
+      setPrices(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error fetching prices:", error);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <View
       style={{
@@ -8,8 +26,16 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    > 
+    {loading ? (
+      <ActivityIndicator size="large" color="#0000ff" />
+    ) : (
+      prices.map((price, index) => (
+        <Text key={index} style={{ fontSize: 18, marginBottom: 10 }}>
+          {new Date(price.time_start).toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })}: {Math.round(price.SEK_per_kWh * 100)} öre/kWh
+        </Text>
+        ))
+      )}
     </View>
   );
 }
