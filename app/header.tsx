bb6da35ba09e11cfbb5712e35253821dbbec4165
Adam <Adam.sturesson@live.se>
2025-03-20 20:42:07 +0100

    styles for the picture in the header

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,27 @@
-import { Image } from "react-native";
+import { View, Image, StyleSheet  } from "react-native";
 
 export default function Header() {
   return (
-    <Image 
-        source={require("../assets/images/CurrentPrice.png")} 
-        style={{ width: "100%", height: 100, resizeMode: "contain" }} 
-    />
+    <View style={styles.headerContainer}>
+        <Image 
+            source={require("../assets/images/CurrentPrice.png")} 
+            style={styles.headerImage}
+        />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+      position: "absolute",
+      top: 0,
+      width: "100%",
+      height: 100, 
+      zIndex: 10, 
+    },
+    headerImage: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "cover",
+    },
+  });
