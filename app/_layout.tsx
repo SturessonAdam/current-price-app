ee3392a74624425137675cc8854ca7081c7c18cb
Adam <Adam.sturesson@live.se>
2025-03-22 22:32:50 +0100

    backround color for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Stack, Tabs } from "expo-router";
+import { StyleSheet, View } from "react-native";
 import Header from "./header";
 
 export default function RootLayout() {
   return (
-  <>
-    <Header/>
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Todays spotprices" }} />
-      <Tabs.Screen name="tomorrow" options={{ title: "Tomorrows spotprices" }} />
-    </Tabs>
-  </>
+    <View style={styles.container}>
+      <Header/>
+      <Tabs>
+        <Tabs.Screen name="index" options={{ title: "Todays spotprices" }} />
+        <Tabs.Screen name="tomorrow" options={{ title: "Tomorrows spotprices" }} />
+      </Tabs>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212"
+  },
+});
+
