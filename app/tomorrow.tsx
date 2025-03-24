29489627fb276a7225aa9b93133e4e8106fd0768
Adam <Adam.sturesson@live.se>
2025-03-24 20:37:52 +0100

    refactor Tomorrow component to include loading indicator and display prices in a scrollable view

--- app/tomorrow.tsx
+++ app/tomorrow.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, ScrollView } from "react-native";
 import { fetchTomorrowsPrices } from "./api/api";
 
 
-export default function tomorrow() {
+export default function Tomorrow() {
   const [prices, setPrices] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   
@@ -19,7 +19,29 @@ export default function tomorrow() {
     
   return (
     <View style={{ flex: 1, paddingTop: 50}}>
-        <Text style={{color: "black"}}>Here should tomorrows spotprices render</Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <ScrollView style={{ flex: 1, maxHeight: 600 }} contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 20, color: "black" }}>Tomorrows spotprices per hour</Text>
+          {prices
+            .sort((a, b) => new Date(a.time_start).getTime() - new Date(b.time_start).getTime())
+            .map((price, index) => {
+              const time = new Date(price.time_start).toLocaleTimeString("sv-SE", {
+                hour: "2-digit",
+                minute: "2-digit",
+              });
+  
+              const priceColor = price.SEK_per_kWh < 0 ? "red" : "black";
+  
+              return (
+                <Text key={index} style={{ fontSize: 20, marginBottom: 5, color: priceColor }}>
+                  {time} → {price.SEK_per_kWh.toFixed(5)} SEK/kWh
+                </Text>
+              );
+            })}
+        </ScrollView>
+      )}
     </View>
   );
 }
