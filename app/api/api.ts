6538a620c979cb5ae5146107b34bad0ff6d7cc4c
Adam <Adam.sturesson@live.se>
2025-03-29 10:20:42 +0100

    added comment in api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,10 +1,12 @@
 //Logic for the api calls
 
+//region is 3 by default
 export async function fetchTodaysPrices() {
     const response = await fetch("http://192.168.68.108:8080/prices");
     return await response.json();
 }
 
+//region is 3 by default
 export async function fetchTomorrowsPrices() {
     const response = await fetch("http://192.168.68.108:8080/prices/tomorrow");
     return await response.json();
