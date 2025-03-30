c45ddabbc11f353a10aa01d8984db7ca754b76f3
Adam <Adam.sturesson@live.se>
2025-03-30 19:04:04 +0200

    added region in the api calls in api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,14 +1,13 @@
 //Logic for the api calls
 
 //region is 3 by default
-export async function fetchTodaysPrices() {
-    const response = await fetch("http://192.168.68.108:8080/prices");
+export async function fetchTodaysPrices(region: string = "3") {
+    const response = await fetch(`http://192.168.68.108:8080/prices?region=${region}`);
     return await response.json();
 }
 
 //region is 3 by default
-export async function fetchTomorrowsPrices() {
-    const response = await fetch("http://192.168.68.108:8080/prices/tomorrow");
+export async function fetchTomorrowsPrices(region: string = "3") {
+    const response = await fetch(`http://192.168.68.108:8080/prices/tomorrow?region=${region}`);
     return await response.json();
 }
-  
