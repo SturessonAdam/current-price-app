cce5e92cf8acd51eb0272e069412fe827fae2f13
Adam <Adam.sturesson@live.se>
2025-04-12 22:23:58 +0200

    remove default region value from fetchTodaysPrices and fetchTomorrowsPrices functions

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,13 +1,10 @@
-//Logic for the api calls
 
-//region is 3 by default
-export async function fetchTodaysPrices(region: string = "3") {
+export async function fetchTodaysPrices(region: string) {
     const response = await fetch(`http://192.168.68.108:8080/prices?region=${region}`);
     return await response.json();
 }
 
-//region is 3 by default
-export async function fetchTomorrowsPrices(region: string = "3") {
+export async function fetchTomorrowsPrices(region: string) {
     const response = await fetch(`http://192.168.68.108:8080/prices/tomorrow?region=${region}`);
     return await response.json();
 }
