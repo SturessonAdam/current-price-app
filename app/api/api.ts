e8d17aaa4d55626a2777a309a87d84d7360770f4
Adam <Adam.sturesson@live.se>
2025-05-01 18:44:16 +0200

    Add dotenv support and update API calls to use environment variables

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,21 +1,34 @@
 import Constants from "expo-constants";
+import { API_BASE_URL } from "@env";
 
-const getBaseUrl = () => {
-  const debuggerHost =
-    Constants.expoConfig?.hostUri?.split(":")[0] ||
-    Constants.manifest2?.extra?.debuggerHost?.split(":")[0]; //fallback om inte expoConfig finns
+// const getBaseUrl = () => {
+//   const debuggerHost =
+//     Constants.expoConfig?.hostUri?.split(":")[0] ||
+//     Constants.manifest2?.extra?.debuggerHost?.split(":")[0]; //fallback om inte expoConfig finns
 
-  return `http://${debuggerHost}:8080`;
-};
+//   return `http://${debuggerHost}:8080`;
+// };
+
+// export async function fetchTodaysPrices(region: string) {
+//     const baseUrl = getBaseUrl();
+//     const response = await fetch(`${baseUrl}/prices?region=${region}`);
+//     return await response.json();
+// }
+
+// export async function fetchTomorrowsPrices(region: string) {
+//     const baseUrl = getBaseUrl();
+//     const response = await fetch(`${baseUrl}/prices/tomorrow?region=${region}`);
+//     return await response.json();
+// }
+
+
 
 export async function fetchTodaysPrices(region: string) {
-    const baseUrl = getBaseUrl();
-    const response = await fetch(`${baseUrl}/prices?region=${region}`);
-    return await response.json();
+  const response = await fetch(`${API_BASE_URL}/prices?region=${region}`);
+  return await response.json();
 }
 
 export async function fetchTomorrowsPrices(region: string) {
-    const baseUrl = getBaseUrl();
-    const response = await fetch(`${baseUrl}/prices/tomorrow?region=${region}`);
-    return await response.json();
+  const response = await fetch(`${API_BASE_URL}/prices/tomorrow?region=${region}`);
+  return await response.json();
 }
